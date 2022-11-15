c308e908235c3f0d8580b2b986fe0758612936c4
ankitpetjay1 <118140607+ankitpetjay1@users.noreply.github.com>
2022-11-15 08:44:51 +0530

    Update Prac8b.py

--- art/Prac8b.py
+++ art/Prac8b.py
@@ -1,3 +1,5 @@
+# Solve constraint satisfaction problem
+
 from __future__ import print_function
 from simpleai.search import CspProblem, backtrack, min_conflicts, MOST_CONSTRAINED_VARIABLE, HIGHEST_DEGREE_VARIABLE, LEAST_CONSTRAINING_VALUE
 
